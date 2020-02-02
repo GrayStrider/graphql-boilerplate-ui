ca76da70ece57ff749a66f411432927fa2040d3c
Gray Strider <zhoga.ivan@gmail.com>
2020-02-02 11:27:58 +1100

    :sparkles: delay request in Apollo Server

--- src/plugins/vueApollo.ts
+++ src/plugins/vueApollo.ts
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, {HttpLink} from 'apollo-boost'
+import sleep from '@utils/sleep'
 
 Vue.use(VueApollo)
 
 const apolloClient = new ApolloClient({
-	uri: 'https://api.spacex.land/graphql/'
+	uri: 'https://api.spacex.land/graphql/',
+	request: operation => sleep(2000),
 })
+
+const httpLink = new HttpLink({uri: 'https://api.spacex.land/graphql/'})
 
 const apolloProvider = new VueApollo({
 	defaultClient: apolloClient,
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
 const {paths} = require('./tsconfig.json').compilerOptions
 const {forEachObjIndexed} = require('ramda')
-async function sleep (ms) {
-  return new Promise(resolve => setTimeout(() => resolve(undefined), ms))
-}
+
 const aliasPaths = (config) => {
   const getStr = (str) => str.replace(/\/.+/, '')
   forEachObjIndexed(([value], key) => {
@@ -13,18 +11,6 @@ const aliasPaths = (config) => {
 
 }
 
-const asyncHandler = fn => (req, res, next) =>
-  Promise
-    .resolve(fn(req, res, next))
-    .catch(next)
-
-const delayRequest = asyncHandler(async (req, res, next) => {
-  console.log('sleeping: ', req.xnr)
-  await sleep(2000)
-  next();
-})
-
-
 module.exports = {
   "lintOnSave": false,
   "transpileDependencies": [
@@ -32,9 +18,6 @@ module.exports = {
   ],
   devServer: {
     host: '127.0.0.1',
-    setup: (app) => {
-      app.use(delayRequest)
-    }
   },
   chainWebpack: config => {
     aliasPaths(config)
