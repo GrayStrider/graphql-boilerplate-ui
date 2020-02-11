246e626e59e5e0a3d8747109d4260658e87ae33a
Gray Strider <zhoga.ivan@gmail.com>
2020-02-12 01:53:50 +1100

    :art: Contacts format

--- src/components/GoogleContacts.vue
+++ src/components/GoogleContacts.vue
@@ -3,35 +3,54 @@
 		v-navigation-drawer(v-model='drawer' :clipped='$vuetify.breakpoint.lgAndUp' app)
 			v-list(dense)
 				template(v-for='item in items')
-					v-row(v-if='item.heading' :key='item.heading' align='center')
+					v-row(
+						v-if='item.heading'
+						:key='item.heading'
+						align='center')
 						v-col(cols=6)
 							v-subheader(v-if='item.heading') {{ item.heading }}
 						v-col.text-center(cols='6')
 							a.body-2.black--text(href='/#!') EDIT
-					v-list-group(v-else-if='item.children' :key='item.text' v-model='item.model' :prepend-icon="item.model ? item.icon : item['icon-alt']")
+					v-list-group(
+						v-else-if='item.children'
+						:key='item.text'
+						v-model='item.model'
+						:prepend-icon="item.model ? item.icon : item['icon-alt']")
 						template(v-slot:activator)
 							v-list-item-content
-								v-list-item-title
-									| {{ item.text }}
-						v-list-item(v-for='(child, i) in item.children' :key='i' link)
+								v-list-item-title {{ item.text }}
+						v-list-item(
+							v-for='(child, i) in item.children'
+							:key='i'
+							link)
 							v-list-item-action(v-if='child.icon')
 								v-icon {{ child.icon }}
 							v-list-item-content
-								v-list-item-title
-									| {{ child.text }}
+								v-list-item-title {{ child.text }}
 					v-list-item(v-else :key='item.text' link)
 						v-list-item-action
 							v-icon {{ item.icon }}
 						v-list-item-content
-							v-list-item-title
-								| {{ item.text }}
-		v-app-bar(:clipped-left='$vuetify.breakpoint.lgAndUp' app color='blue darken-3' dark)
+							v-list-item-title {{ item.text }}
+		v-app-bar(
+			:clipped-left='$vuetify.breakpoint.lgAndUp'
+			app
+			color='blue darken-3'
+			dark)
 			v-app-bar-nav-icon(@click.stop='drawer = !drawer')
 			v-toolbar-title.ml-0.pl-4(style='width: 300px')
 				span.hidden-sm-and-down Google Contacts
-			v-text-field.hidden-sm-and-down(flat solo-inverted hide-details prepend-inner-icon='mdi-magnify' label='Search')
+			v-text-field.hidden-sm-and-down(
+				flat
+				solo-inverted
+				hide-details
+				prepend-inner-icon='mdi-magnify'
+				label='Search')
 				v-spacer
-					v-switch(hide-details label='Dark Mode' v-model='$vuetify.theme.dark')
+					v-switch(
+						hide-details
+						label='Dark Mode'
+						v-model='$vuetify.theme.dark')
 					v-btn(icon)
 						v-icon mdi-apps
 					v-btn(icon)
@@ -43,7 +62,14 @@
 			v-container.fill-height(fluid)
 				v-row(align='center' justify='center')
 					ShipsTable
-		v-btn(bottom right fixed color='pink' dark fab @click='dialog = !dialog')
+		v-btn(
+			bottom
+			right
+			fixed
+			color='pink'
+			dark
+			fab
+			@click='dialog = !dialog')
 			v-icon mdi-plus
 		v-dialog(v-model='dialog' width='800px')
 			v-card
@@ -66,30 +92,21 @@
 						v-col(cols='12')
 							v-text-field(prepend-icon='mdi-text' placeholder='Notes')
 				v-card-actions
-					v-btn(text color='primary')
-						| More
+					v-btn(text color='primary') More
 					v-spacer
-						v-btn(text color='primary' @click='dialog = false')
-							| Cancel
-						v-btn(text @click='dialog = false')
-							| Save
+						v-btn(text color='primary' @click='dialog = false') Cancel
+						v-btn(text @click='dialog = false') Save
 </template>
 
 <script>
-
   import ShipsTable from "@/components/ShipsTable";
 
   export default {
     components: {ShipsTable},
-
-
     props: {
       source: String,
     },
-
-
     data: () => ({
-
       dialog: false,
       drawer: null,
       items: [
